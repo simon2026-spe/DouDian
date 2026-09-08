983f4cc12957e7d0ce94128c93685b704dfd095f
simon2026-spe <simon2026-spe@users.noreply.github.com>
2026-09-08 15:05:46 +0800

    Fix: 搜索栏输入框对齐问题，改用flex布局

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import { Form, Input, Select, Button, Space, Row, Col } from 'antd'
+import { Form, Input, Select, Button, Space } from 'antd'
 import { SearchOutlined, ReloadOutlined } from '@ant-design/icons'
+import './SearchBar.css'
 
-/**
- * 搜索筛选栏组件
- * @param {array} fields - 搜索字段配置
- * @param {function} onSearch - 搜索回调
- * @param {function} onReset - 重置回调
- * @param {ReactNode} extra - 额外操作按钮
- */
 const SearchBar = ({ fields = [], onSearch, onReset, extra, initialValues = {} }) => {
   const [form] = Form.useForm()
 
   const handleSearch = () => {
     const values = form.getFieldsValue()
-    // 过滤掉空值
     const filteredValues = {}
     Object.keys(values).forEach((key) => {
       if (values[key] !== undefined && values[key] !== null && values[key] !== '') {
@@ -29,7 +22,6 @@ const SearchBar = ({ fields = [], onSearch, onReset, extra, initialValues = {} }
     onReset && onReset()
   }
 
-  // 渲染表单项
   const renderField = (field) => {
     const { name, label, type = 'input', options = [], placeholder, ...rest } = field
 
@@ -52,7 +44,7 @@ const SearchBar = ({ fields = [], onSearch, onReset, extra, initialValues = {} }
         break
       case 'select':
         component = (
-          <Select placeholder={getPlaceholder()} allowClear {...rest}>
+          <Select placeholder={getPlaceholder()} allowClear style={{ width: '100%' }} {...rest}>
             {options.map((opt) => (
               <Select.Option key={opt.value} value={opt.value}>
                 {opt.label}
@@ -66,37 +58,33 @@ const SearchBar = ({ fields = [], onSearch, onReset, extra, initialValues = {} }
     }
 
     return (
-      <Form.Item name={name} label={label} key={name} style={{ marginBottom: 0 }}>
+      <div key={name} className="searchbar-item">
+        <label className="searchbar-label">{label}：</label>
         {component}
-      </Form.Item>
+      </div>
     )
   }
 
   return (
     <Form
       form={form}
-      layout="inline"
       initialValues={initialValues}
-      style={{ marginBottom: 16 }}
+      className="searchbar-container"
     >
-      <Row gutter={[16, 16]} style={{ width: '100%' }}>
-        {fields.map((field) => (
-          <Col key={field.name} xs={24} sm={12} md={8} lg={6} xl={4}>
-            {renderField(field)}
-          </Col>
-        ))}
-        <Col xs={24} sm={12} md={8} lg={6} xl={4} style={{ textAlign: 'right' }}>
-          <Space>
-            <Button type="primary" icon={<SearchOutlined />} onClick={handleSearch}>
-              搜索
-            </Button>
-            <Button icon={<ReloadOutlined />} onClick={handleReset}>
-              重置
-            </Button>
-            {extra}
-          </Space>
-        </Col>
-      </Row>
+      <div className="searchbar-fields">
+        {fields.map((field) => renderField(field))}
+      </div>
+      <div className="searchbar-actions">
+        <Space>
+          <Button type="primary" icon={<SearchOutlined />} onClick={handleSearch}>
+            搜索
+          </Button>
+          <Button icon={<ReloadOutlined />} onClick={handleReset}>
+            重置
+          </Button>
+          {extra}
+        </Space>
+      </div>
     </Form>
   )
 }
